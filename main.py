075e0590636cd6c3aaff9bd23c6f3cbf3de2aa44
KentonMewling <tjwright1228@gmail.com>
2023-09-20 12:18:08 -0700

    got the correct data from the messy json api :

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from keys import consumer_key, consumer_secret, access_token, access_token_secre
 import requests
 import json
 from datetime import date
-
+import pandas as pd
 #today = date.today()
 #today = today.strftime("%Y-%m-%d")
 
@@ -11,11 +11,33 @@ today = '2018-01-02'
 
 
 print(today)
-todaysGames = requests.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate=' + today + '&endDate=' + today).text
-jsonData = json.loads(todaysGames)
-todaysnumGames = jsonData['totalGames']
+todaysData = requests.get('https://statsapi.web.nhl.com/api/v1/schedule?startDate=' + today + '&endDate=' + today).text
+jsonData = json.loads(todaysData)
 
-print(todaysnumGames)
+jsonData = jsonData['dates'][0]['games']
+
+for team in jsonData:
+    teamList = team['teams']['away']['team']['name']
+    print(json.dumps(teamList, indent=4))
+
+    # for awayList in teamList:
+    #     awayLists = awayList['team']
+    #     for awayTeam in awayLists:
+    #         awayTeamDetail = awayTeam['team']
+    #         for awayTeamName in awayTeamDetail:
+    #             print(awayTeamName['name'])
+    
+
+#   for awayTeam in teamList:
+#     awayTeamName = awayTeam['away']
+#     for awayTeamFinalname in awayTeamName:
+                
+
+
+
+
+
+# print(todaysnumGames)
 
 # #V1 Endpoint 
 # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
