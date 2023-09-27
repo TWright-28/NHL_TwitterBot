b532043373b2ee7c8092b1d96a7b7c0205126ec9
KentonMewling <tjwright1228@gmail.com>
2023-09-26 20:51:32 -0700

    created file cleaner to remove the png's at the end of the night

---
+++ fileCleaner.py
@@ -0,0 +1,18 @@
+import os
+
+# Define the folder path where you want to delete .png files
+folder_path = '../NHL_TwitterBot'
+
+# Iterate through the files in the folder
+for filename in os.listdir(folder_path):
+    # Check if the file ends with ".png"
+    if filename.endswith(".pdf"):
+        # Construct the full file path
+        file_path = os.path.join(folder_path, filename)
+        
+        # Try to remove the file
+        try:
+            os.remove(file_path)
+            print(f"Deleted: {filename}")
+        except Exception as e:
+            print(f"Failed to delete {filename}: {str(e)}")
